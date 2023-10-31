add686336da0ac0b86ae0d0e2a2ecacd13df83fe
Mauricio <mauriciodiego2019@gmail.com>
2023-10-31 10:56:42 -0500

    tabla hecha

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: []
+    }
+    
+}
+componentWillMount() {
+  fetch('http://localhost:8000/api/producto/')
+    .then((response) => {
+      return response.json()
+    })
+    .then((prod) => {
+      this.setState({ productos: prod })
+    })    
+} 
+render() {
+  return (
+    <div>
+      <table border="1">
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Descripción</th>
+          <th>Precio</th>                    
+        </tr>
+      </thead>
+      <tbody>  
+        {this.state.productos.map(producto => {
+          return (
+            <tr key={producto.codigo}>
+              <td>{producto.codigo}</td>
+              <td>{producto.descripcion}</td>
+              <td>{producto.precio}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+      </table>
+    </div>
+  );
+}
+
+}
+/* function App(){
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +67,7 @@ function App() {
       </header>
     </div>
   );
-}
+  
+} */
 
 export default App;
